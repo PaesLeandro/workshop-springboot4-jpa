4a875a3be8b45f42ade1f9c679e3217325e42cc3
Leandro <leandromkt.tr@gmail.com>
2025-12-27 22:32:02 -0300

    Service layer, component registration

---
+++ src/main/java/com/paestech/course/services/UserServices.java
@@ -0,0 +1,28 @@
+package com.paestech.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paestech.course.entities.User;
+import com.paestech.course.repositories.UserRepository;
+
+
+@Service
+public class UserServices {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long Id) {
+		Optional<User> obj = repository.findById(Id);
+		return obj.get();
+	}
+
+}
