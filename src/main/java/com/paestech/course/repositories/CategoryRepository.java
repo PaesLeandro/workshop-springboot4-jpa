1a0ed553f22f7754505ee07ca160fceeb06323ad
Leandro <leandromkt.tr@gmail.com>
2025-12-31 17:20:21 -0300

    Category

--- src/main/java/com/paestech/course/repositories/CategoryRepository.java
+++ src/main/java/com/paestech/course/repositories/CategoryRepository.java
@@ -2,8 +2,11 @@ package com.paestech.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.paestech.course.entities.Category;
 import com.paestech.course.entities.Order;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
+
