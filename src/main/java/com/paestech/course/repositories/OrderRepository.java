ab53a50542f9fca2b5cfa432f07ee3d5f19dd9e4
Leandro <leandromkt.tr@gmail.com>
2025-12-31 15:01:32 -0300

    Order, Instant, ISO 8601 - Part 2

---
+++ src/main/java/com/paestech/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.paestech.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.paestech.course.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
+
